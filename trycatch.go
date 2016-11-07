dc9c643bb55a40e652a8eb124e964da93d3f01b7
shenjiayu <sjy19930312@gmail.com>
2016-11-08 10:44:14 +1100

    new branch

--- trycatch.go
+++ trycatch.go
@@ -1,6 +1,7 @@
 package trycatch
 
 // simple implementation of try catch in golang
+// this is a dev branch
 func Try(fun func(), catch func(interface{})) {
 	defer func() {
 		if err := recover(); err != nil {
