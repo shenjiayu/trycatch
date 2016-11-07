3db963392b35ac3d7b135dc29c1cae0ade24e882
shenjiayu <sjy19930312@gmail.com>
2016-11-08 10:47:17 +1100

    clarification

--- trycatch.go
+++ trycatch.go
@@ -1,6 +1,7 @@
 package trycatch
 
 // simple implementation of try catch in golang
+// this is the master branch
 func Try(fun func(), catch func(interface{})) {
 	defer func() {
 		if err := recover(); err != nil {
