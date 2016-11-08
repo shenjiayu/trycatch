d5e210122544feefc8a1482a196b5aeb9ffeb8d4
shenjiayu <sjy19930312@gmail.com>
2016-11-08 11:07:29 +1100

    conflicts

--- trycatch.go
+++ trycatch.go
@@ -2,6 +2,7 @@ package trycatch
 
 // simple implementation of try catch in golang
 // this is a dev branch
+// resolve conflicts
 func Try(fun func(), catch func(interface{})) {
 	defer func() {
 		if err := recover(); err != nil {
