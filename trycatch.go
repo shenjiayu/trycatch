4ab26d4ed1f5ff276f95d2ef05824b3b1c6e40d8
shenjiayu <sjy19930312@gmail.com>
2016-12-06 20:29:47 +1100

    push

--- trycatch.go
+++ trycatch.go
@@ -2,6 +2,7 @@ package trycatch
 
 // simple implementation of try catch in golang
 // hahaha
+// what?
 func Try(fun func(), catch func(interface{})) {
 	defer func() {
 		if err := recover(); err != nil {
