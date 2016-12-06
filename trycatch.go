5564468247e8842e52f948c5edb16728100b42a8
shenjiayu <sjy19930312@gmail.com>
2016-12-06 19:07:24 +1100

    nothing

--- trycatch.go
+++ trycatch.go
@@ -1,10 +1,6 @@
 package trycatch
 
 // simple implementation of try catch in golang
-// testing the merge function
-// this is a master branch
-// resolve conflicts
-// testing vscode editor
 func Try(fun func(), catch func(interface{})) {
 	defer func() {
 		if err := recover(); err != nil {
